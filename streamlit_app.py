acf23ff95dbe41b9c36c7c71932e80c1fbfc1b38
MagaliDrumare <mag250475@gmail.com>
2022-03-31 18:06:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,11 +25,11 @@ def run_query():
         workbooks_names = [w.name for w in workbooks]
 
         # Get views for first workbook.
-        server.workbooks.populate_views(workbooks[23])
-        views_names = [v.name for v in workbooks[23].views]
+        server.workbooks.populate_views(workbooks[25])
+        views_names = [v.name for v in workbooks[25].views]
 
         # Get image & CSV for first view of first workbook.
-        view_item = workbooks[23].views[0]
+        view_item = workbooks[25].views[0]
         server.views.populate_image(view_item)
         server.views.populate_csv(view_item)
         view_name = view_item.name
